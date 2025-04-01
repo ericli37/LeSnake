18ed2cde0aefba262a22fe081e8a948315aa790d
ericli37 <li.eric.w@gmail.com>
2025-04-01 11:12:58 -0500

    day 2

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ document.getElementById('playButton').addEventListener('click', () => {
     document.getElementById('game').style.display = 'block';
 });
 
-const playBoard = document.getElementById('play-board');
+const playBoard = document.querySelector('.play-board');
 
-let foodX = 13
+let foodX = 13, foodY = 10;
+
+const initGame = () => {
+    let htmlMarkup = `<div class ="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+    playBoard.innerHTML = htmlMarkup;
+}
+
+initGame();
